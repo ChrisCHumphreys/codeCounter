4c53a3f6035e22e71598c93d325874276d0ef7a3
ChrisCHumphreys <geochris35@gmail.com>
2018-09-29 00:06:39 -0500

    pushing so brayden can see

---
+++ create_lambda_package.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+BASEDIR=$(pwd)
+rm -rf $BASEDIR/lambda_package.zip
+cd  $BASEDIR/lambda/codeCounterEnv/lib/python3.6/site-packages/
+zip -r9 $BASEDIR/lambda_package.zip *
+cd $BASEDIR/lambda/codeCounterEnv/lib64/python3.6/site-packages/
+zip -r9 $BASEDIR/lambda_package.zip *
+cd $BASEDIR/lambda
+
+zip -r9 $BASEDIR/lambda_package.zip codeCounter_classes_lambda.py
